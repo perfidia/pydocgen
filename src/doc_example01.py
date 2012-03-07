cdf55cbfd1b5d4cc8257f5783d8e204b47ad75e5
Blazej Matuszyk <blazej.matuszyk@student.put.poznan.pl>
2012-03-07 16:11:08 +0100

    - started creating the example document

--- src/doc_example01.py
+++ src/doc_example01.py
@@ -1,3 +1,42 @@
-from pydocgen.model import *
+# -*- coding: utf-8 -*-
 
-d = Document()
+from pydocgen.model import *
+from pydocgen.builders.latex import LatexBuilder
+from pydocgen.builders.html import HtmlBuilder
+
+document = Document()
+
+document.properties['lang'] = "pl"
+document.style['margin-top'] = 20
+document.style['margin-right'] = 20
+document.style['margin-left'] = 20
+document.style['margin-bottom'] = 10
+document.style['font-size'] = 12
+
+headers_seq = Sequence()
+
+span = Span("Phasellus tempor risus eget.")
+header = Header()
+header.sequence = headers_seq
+header += span
+
+document += header
+
+span = Span("Sed in purus dolor. Ut id mauris vel urna fringilla blandit. Phasellus non risus dolor. Curabitur convallis dolor ac massa sollicitudin dapibus. Phasellus nulla neque, vestibulum eget gravida vel,euismod eget lacus. Sed in purus dolor. Ut id mauris vel urna fringilla blandit. Phasellus non risus dolor. Curabitur convallis dolor ac massa sollicitudin dapibus. Phasellus nulla neque, vestibulum eget gravida vel, euismod eget lacus.")
+paragraph = Paragraph()
+paragraph += span
+
+document += paragraph
+
+#...
+#TODO
+
+
+# here we generate the document
+
+if __name__ == '__main__':
+    document.builder = LatexBuilder()
+    document.generate_file("doc_example01.tex")
+    
+    document.builder = HtmlBuilder()
+    document.generate_file("doc_example01.htm")
