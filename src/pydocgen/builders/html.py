0668a50a3c8f9837d24eafd74c852630bd8fe75a
bmitan <bmitan@gmail.com>
2012-03-11 23:50:59 +0100

    by pawel- html.py

--- src/pydocgen/builders/html.py
+++ src/pydocgen/builders/html.py
@@ -6,42 +6,128 @@ from pydocgen.builders.common import Builder
 class HtmlBuilder(Builder):
     def __init__(self):
         super(HtmlBuilder, self).__init__()
+        self.CSS_STYLE_FN = 'style.css'
     
     def generate_document(self, document):
-    	MAIN_STYLE_FILENAME = 'style.css'
     	title = ''
 	body = ''
-	if 'title' in document.properties:
-		title = document.properties['title']
-	for element in document:
-		body += generate(element)
-	self.generate_main_style(MAIN_STYLE_FILENAME)
-        return '<html><head>\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"' + MAIN_STYLE_FILENAME + '\" />\n<title>'+ title +'</title>\n\t</head>\n\t<body>\n'+ body  +'\n\t</body>\n</html>'
+	for element in document.content:
+		body += self.generate(element)
+	self.generate_style_file(document.effective_style, self.CSS_STYLE_FN)
+	result =''
+	if 'lang' in document.properties:
+	    result += '<html lang=\"'+document.properties['lang']+'\">'
+	else:
+	    result += '<html>\n'
+        result += '<head>\n'
+        result += '\t<title>'+ document.title +'</title>\n'
+        result += '\t<link rel=\"stylesheet\" type=\"text/css\" href=\".\\'+self.CSS_STYLE_FN+'\" />\n'
+        result += '</head>\n\n'
+        result += '<body>\n'+ body +'\n</body>\n</html>'
+        return result
     
     def generate_paragraph(self, paragraph):
-	p = generate(p)
-        return '<p>' + p + '</p>'
+    	p, tmp = '', None
+    	if paragraph.content:
+	    for element in paragraph.content:		
+	        tmp = self.generate(element)
+	        if tmp :
+	            p += tmp
+        return '\n<p '+self.generate_inline_style(paragraph)+'>\n\t' + p + '\n</p>\n'
     
     def generate_span(self, span):
-        return '<span>' + s.text + '</span>'
+        return '<span '+self.generate_inline_style(span)+'>' + span.text + '</span>'
     
     def generate_header(self, header):
-        return '<h1>' + header.sequence.value + '</h1>' 
+        content = ''
+    	if header.content:
+    	    for element in content:
+    	        if element:
+    	            content += generate(element)
+        return '\n\n<h1 '+self.generate_inline_style(header)+'>' + header.sequence.to_str() + content + '</h1>\n\n' 
     
     def generate_list(self, lst):
-	result  = ''
-	for item in lest.content:
-	    result += generate(lst.content)
+	result, tmp  = '', None
+	for item in lst.content:
+	    tmp = self.generate(item)
+	    if tmp:
+	    	result += '\n<li '+self.generate_inline_style(tmp)+'>'+ tmp +'</li>\n'
 	if lst.style['list-style'] == ListStyle.BULLET:
-	    return '<ul>' + result + '</ul>'
+	    return '\n<ul '+self.generate_inline_style(lst)+'>\n' + result + '\n</ul>\n'
 	else:
-	    return '<ol>' + result + '</ol>'
+	    return '\n<ol '+self.generate_inline_style(lst)+'>\n' + result + '\n</ol>\n'
     
     def generate_image(self, image):
-        return '<a src=\"'+ image.path + '\" alt=\"' + image.sequence.to_str() + '\" />'
+        return '<img src=\"' + image.path + ' '+self.generate_inline_style(element)+'\" />'
     
     def generate_table(self, table):
-        return ""
+    	result = '<table border=\"1\">'
+	for i in xrange(0, table.rows_num - 1):
+	    for j in xrange(0, table.cols_num - 1):
+	        result += self.generate( table.get_cell(i, j) )
+        return  result  + '</table>'
         
-    def generate_main_style(self, filename):
-    	pass
+    def generate_style_file(self, style, fn):
+	css = 'body {\n'
+   	if style != None:
+   	    for key in style.keys():
+		if key in ('margin-top', 'margin-bottom', 'margin-left', 'margin-right','font-size','font-name','alignment','text-indent','color','background-color','list-_style','item-spacing','item-indent'):
+		    if key == 'font-name':
+		        css += 'font-family: ' + style[key] + ';\n'
+	            elif key == 'alignment':
+	                css += 'text-align: '+{Alignment.LEFT:'left',Alignment.CENTER:'center',Alignment.RIGHT:'right',Alignment.JUSTIFY:'justify'}.get(style[key])+';\n'
+	            elif  key == 'list-_style':
+	            	pass #Using <ul> or <ol> instead
+	            elif key == 'font-effect':
+	                css += 'font-style: '+{FontEffect.BOLD:'bold',FontEffect.ITALIC:'italic',FontEffect.UNDERLINE:'oblique'}.get(style[key])+';\n'
+	            elif key == 'item-spacing':
+	                css += 'border-spacing: '+str(style[key])+'pt '+str(style[key])+'pt;\n letter-spacing: '+str(style[key])+'pt;\n word-spacing: '+str(style[key])+'pt;\n';
+	            elif key == 'item-indent':
+	                pass #css += 'text-indent: ' + str(style[key]) + 'pt;\n'
+	            elif key == 'background-color' or key == 'color':
+	                css += key + ': ' + style[key] + ';\n'
+		    else:
+		        css += key + ': ' + str(style[key]) + 'pt;\n'
+	    css = css[:-2]
+	    css += '\n}\n'
+	    
+	    output_file = open(fn, "w")
+            output_file.write(css)
+            output_file.close() 
+   	return None
+   	
+    def generate_inline_style(self, elem):
+	result = ''
+	try:
+	    if isinstance(elem.style, Style) :
+	        if elem.__class__.style != elem.style:
+	            result += self.__generate_style_from_dict(elem.style)
+	except:
+	    result = ''
+	return result
+
+   	    
+    def __generate_style_from_dict(this, style):
+	  css = ''
+	  if style != None:
+   	    css = 'style = \"'
+   	    for key in style.keys():
+		if key in ('margin-top', 'margin-bottom', 'margin-left', 'margin-right','font-size','font-name','alignment','text-indent','color','background-color','list-_style','item-spacing','item-indent'):
+		    if key == 'font-name':
+		        css += 'font-family: ' + style[key] + ';'
+	            elif key == 'alignment':
+	                css += 'text-align: '+{Alignment.LEFT:'left',Alignment.CENTER:'center',Alignment.RIGHT:'right',Alignment.JUSTIFY:'justify'}.get(style[key])+';'
+	            elif  key == 'list-_style':
+	            	pass #Using <ul> or <ol> instead
+	            elif key == 'font-effect':
+	                css += 'font-style: '+{FontEffect.BOLD:'bold',FontEffect.ITALIC:'italic',FontEffect.UNDERLINE:'oblique'}.get(style[key])+';'
+	            elif key == 'item-spacing':
+	                css += 'border-spacing: '+str(style[key])+'pt '+str(style[key])+'pt; letter-spacing: '+str(style[key])+'pt; word-spacing: '+str(style[key])+'pt;';
+	            elif key == 'item-indent':
+	                pass #css += 'text-indent: ' + str(style[key]) + 'pt;\n'
+	            elif key == 'background-color' or key == 'color':
+	                css += key + ': ' + style[key] + ';'
+		    else:
+		        css += key + ': ' + str(style[key]) + 'pt;'
+	    css += '\"'
+	  return css 
