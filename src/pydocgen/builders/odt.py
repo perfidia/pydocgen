a584e7f01588cfc3102c2beaf4ef6e719ffec76a
hilfman <hilfman@o2.pl>
2013-04-11 00:51:37 +0200

    span styles

--- src/pydocgen/builders/odt.py
+++ src/pydocgen/builders/odt.py
@@ -14,10 +14,21 @@ class OdtBuilder(Builder):
 
     def generate_document(self, document):
         
+        self.styles = dict()
+        self.styleIndex = 0
+        
         body = ''
 
         for element in document.content:
             body += self.generate(element)
+         
+        styles = '    <office:automatic-styles>\n'
+        if self.styles is not None:
+            for key in self.styles.keys():
+                styles += '        <style:style style:name="T' + str(key) + '" style:family="text">\n'
+                styles += '            <style:text-properties ' + self.styles[key] + '/>\n'
+                styles += '        </style:style>\n'
+        styles += '    </office:automatic-styles>\n'
         
         result =  '<?xml version="1.0" encoding="UTF-8"?>\n'
         result += '\n'
@@ -25,6 +36,8 @@ class OdtBuilder(Builder):
         result += '    xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" \n'
         result += '    xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0"\n'
         result += '    xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" \n'
+        result += '    xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0"\n'
+        result += '    xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"\n'
         result += '    office:version="1.2" \n'
         result += '    office:mimetype="application/vnd.oasis.opendocument.text">\n'
         result += '\n'
@@ -32,6 +45,7 @@ class OdtBuilder(Builder):
         result += '        <meta:creation-date>2013-04-09T22:26:39</meta:creation-date>\n'
         result += '        <meta:generator>pydocgen 0.0.3</meta:generator>\n'
         result += '</office:meta>\n'
+        result += styles
         result += '<office:body>\n'
         result += '       <office:text>\n'
         result += body
@@ -56,9 +70,40 @@ class OdtBuilder(Builder):
     
     def generate_span(self, span):
 
-        result = '<text:span>' + span.text + '</text:span>'
+        resolved = self.resolveStyle(span);
+
+        if resolved:
+            result = '<text:span text:style-name="T' + str(self.styleIndex) + '">' + span.text + '</text:span>'
+        else:
+            result = '<text:span>' + span.text + '</text:span>'
+        
         return result
     
+    def resolveStyle(self, elem):
+        if isinstance(elem, str) or isinstance(elem, unicode):
+                return ''
+        style = elem.style
+        
+        self.styleIndex += 1
+        styleBody = ''
+        
+        if style is not None:
+            for key in style.keys():
+                if key == 'font-effect':
+                    font_effects = style['font-effect']
+                    if FontEffectProperty.BOLD in font_effects:
+                        styleBody += 'fo:font-weight="bold" style:font-weight-asian="bold" style:font-weight-complex="bold" '
+                    if FontEffectProperty.ITALIC in font_effects:
+                        styleBody += 'fo:font-style="italic" style:font-style-asian="italic" style:font-style-complex="italic" '
+                    elif FontEffectProperty.UNDERLINE in font_effects:
+                        styleBody += 'style:text-underline-style="solid" style:text-underline-width="auto" style:text-underline-color="font-color" '
+                        
+                    self.styles[self.styleIndex] = styleBody
+                    
+        if styleBody <> '':
+            return True
+        else:
+            return False;
     
     def generate_header(self, el):
         return ''
