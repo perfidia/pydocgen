6c6659e45be5f47f1e77f1318c0b0e3b1ea0637e
mafiagustawa <mafiagustawa@o2.pl>
2013-04-11 02:31:25 +0200

    Minor odt syntax corrections.

--- src/pydocgen/builders/odt.py
+++ src/pydocgen/builders/odt.py
@@ -22,17 +22,17 @@ class OdtBuilder(Builder):
         for element in document.content:
             body += self.generate(element)
          
-        styles = '    <office:automatic-styles>\n'
+        styles = '    <office:document-styles>\n'
         if self.styles is not None:
             for key in self.styles.keys():
                 styles += '        <style:style style:name="T' + str(key) + '" style:family="text">\n'
                 styles += '            <style:text-properties ' + self.styles[key] + '/>\n'
                 styles += '        </style:style>\n'
-        styles += '    </office:automatic-styles>\n'
+        styles += '    </office:document-styles>\n'
         
         result =  '<?xml version="1.0" encoding="UTF-8"?>\n'
         result += '\n'
-        result += '<office:document\n'
+        result += '<office:document-content\n'
         result += '    xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" \n'
         result += '    xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0"\n'
         result += '    xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" \n'
@@ -40,11 +40,12 @@ class OdtBuilder(Builder):
         result += '    xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"\n'
         result += '    office:version="1.2" \n'
         result += '    office:mimetype="application/vnd.oasis.opendocument.text">\n'
+        result += '</office:document-content>\n'
         result += '\n'
-        result += '<office:meta>\n'
+        result += '<office:document-meta>\n'
         result += '        <meta:creation-date>2013-04-09T22:26:39</meta:creation-date>\n'
         result += '        <meta:generator>pydocgen 0.0.3</meta:generator>\n'
-        result += '</office:meta>\n'
+        result += '</office:document-meta>\n'
         result += styles
         result += '<office:body>\n'
         result += '       <office:text>\n'
