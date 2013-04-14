be3f651cd754b92aedac7516369e52c64b718fce
hilfman <hilfman@o2.pl>
2013-04-14 21:28:36 +0200

    odt: some refactoring + alignments

--- src/pydocgen/builders/odt.py
+++ src/pydocgen/builders/odt.py
@@ -19,6 +19,7 @@ class OdtBuilder(Builder):
         self.spanStyles = dict()
         self.paragraphStyles = dict()
         self.styleIndex = 0
+        self.__styleManager = OdtStyleManager(document)
         
         body = ''
         meta= ''
@@ -93,11 +94,7 @@ class OdtBuilder(Builder):
         styleBody = ''
         
         if paragraph.style is not None:
-            styleBody += self.__addAttributeMM(paragraph.style, 'text-indent', 'fo:text-indent')
-            styleBody += self.__addAttributeMM(paragraph.style, 'margin-top', 'fo:margin-top')
-            styleBody += self.__addAttributeMM(paragraph.style, 'margin-bottom', 'fo:margin-bottom')
-            styleBody += self.__addAttributeMM(paragraph.style, 'margin-left', 'fo:margin-left')
-            styleBody += self.__addAttributeMM(paragraph.style, 'margin-right', 'fo:margin-right')
+            styleBody = self.__styleManager.getParagraphStyles(paragraph.style)
 
         if styleBody <> '':
             self.paragraphStyles[self.styleIndex] = styleBody
@@ -108,19 +105,9 @@ class OdtBuilder(Builder):
 
         return result
     
-    def __addAttributeMM(self, style, name, nativeName):
-        return self.__addAttribute(style, name, nativeName, 'mm')
-    
-    def __addAttribute(self, style, name, nativeName, valueSuffix):
-        att = ''
-        if style.has_key(name):
-            value = str(style[name]) + valueSuffix
-            att = nativeName + '="' + value + '" '
-        return att
-    
     def generate_span(self, span):
 
-        resolved = self.__resolveStyle(span);
+        resolved = self.__resolveTextStyle(span);
 
         if resolved:
             result = '<text:span text:style-name="' + str(self.styleIndex) + '">' + span.text + '</text:span>'
@@ -130,7 +117,7 @@ class OdtBuilder(Builder):
         
         return result
     
-    def __resolveStyle(self, elem):
+    def __resolveTextStyle(self, elem):
         if isinstance(elem, str) or isinstance(elem, unicode):
                 return ''
         style = elem.style
@@ -138,17 +125,8 @@ class OdtBuilder(Builder):
         styleBody = ''
    
         if style is not None:
-            for key in style.keys():
-                if key == 'font-effect':
-                    font_effects = style['font-effect']
-                    if FontEffectProperty.BOLD in font_effects:
-                        styleBody += 'fo:font-weight="bold" style:font-weight-asian="bold" style:font-weight-complex="bold" '
-                    if FontEffectProperty.ITALIC in font_effects:
-                        styleBody += 'fo:font-style="italic" style:font-style-asian="italic" style:font-style-complex="italic" '
-                    elif FontEffectProperty.UNDERLINE in font_effects:
-                        styleBody += 'style:text-underline-style="solid" style:text-underline-width="auto" style:text-underline-color="font-color" '
-                        
-                    self.spanStyles[self.styleIndex] = styleBody
+            styleBody = self.__styleManager.getTextStyles(style)     
+            self.spanStyles[self.styleIndex] = styleBody
                     
         if styleBody <> '':
             return True
@@ -175,7 +153,7 @@ class OdtBuilder(Builder):
             else:
                 header.sequence.advance()
         
-        return '        <text:h text:outline-level="' + seq_number + '">' + content + '</text:h>\n'
+        return '        <text:h text:outline-level="' + seq_number + '">' + seq_number + " " + content + '</text:h>\n'
     
     def generate_list(self, el):
         return ''
@@ -188,90 +166,30 @@ class OdtBuilder(Builder):
     
     def __resolveDocumentStyle(self, style):
         return self.__resolvePageLayout(style)
-#        if style != None:
-#            for key in style.keys():
-#                if key == 'font-size':
-#                    pass
-#                elif key == 'font-name':
-#                    pass
-#                elif key == 'alignment':
-#                    pass
-#                elif key == 'text-indent':
-#                    pass
-#                elif key == 'color':
-#                    pass
-#                elif key == 'background-color':
-#                    pass
-#                elif key == 'list-style':
-#                    pass
-#                elif key == 'item-spacing':
-#                    pass
-#                elif key == 'item-indent':
-#                    pass
-#                elif key == 'header-numbered':
-#                    pass
-#                elif key == 'border-width':
-#                    pass
                 
     def __resolvePageLayout(self, style):
-        if style != None:
+        if style is not None:
             styleAttributes = ''
             orientation = None
             size = None
             
-            if style.has_key('page-numbering'):
-                pass # TODO
-            if style.has_key('page-size'):
-                size = style['page-size']
-                if orientation is not None and size is not None:
-                    styleAttributes = self.__getPageSize()
-            if style.has_key('page-orientation'):
-                orientation = style['page-orientation']
-                if orientation == PageOrientationProperty.PORTRAIT:
-                    styleAttributes += 'style:print-orientation="portrait" '
-                else:
-                    styleAttributes += 'style:print-orientation="landscape" '
-                if size is not None and size is not None:
-                    styleAttributes += self.__getPageSize(size, orientation)
-            styleAttributes += self.__addAttributeMM(style, 'margin-top', 'fo:margin-top')
-            styleAttributes += self.__addAttributeMM(style, 'margin-bottom', 'fo:margin-bottom')
-            styleAttributes += self.__addAttributeMM(style, 'margin-left', 'fo:margin-left')
-            styleAttributes += self.__addAttributeMM(style, 'margin-right', 'fo:margin-right')
+            styleAttributes = self.__styleManager.getPageLayoutStyles()
  
             if styleAttributes <> '':
-                styleStr = '<style:page-layout style:name="pm1">\n'
-                styleStr += '<style:page-layout-properties ' + styleAttributes + '/>\n'
-                styleStr += '</style:page-layout>\n'
+                styleStr  = '        <style:page-layout style:name="pm1">\n'
+                styleStr += '            <style:page-layout-properties ' + styleAttributes + '/>\n'
+                styleStr += '        </style:page-layout>\n'
 #                   <style:header-style/> ???
 #                   <style:footer-style/> ???
                 return styleStr
-    
-    def __getPageSize(self, size, orientation):
-        styleAttributes = ''
-        if orientation == PageOrientationProperty.PORTRAIT:
-            styleAttributes += 'fo:page-width="' + str(size.value[0]) + 'mm" '
-            styleAttributes += 'fo:page-height="' + str(size.value[1]) + 'mm" ' 
-        else:
-            styleAttributes += 'fo:page-width="' + str(size.value[1]) + 'mm" '
-            styleAttributes += 'fo:page-height="' + str(size.value[0]) + 'mm" ' 
-            
-        return styleAttributes
-    
-#    <style:page-layout-properties 
-#    fo:page-width="9.9217in" 
-#    fo:page-height="6.9291in" 
-#    style:num-format="1" 
-#    style:print-orientation="portrait" 
-#    style:shadow="none" 
-#    style:writing-mode="lr-tb" 
-#    style:footnote-max-height="0in">
-#   </style:page-layout-properties>
+
+        return ''
 
                 
 #_style[] = PageSizeProperty.A4
 #_style[] = PageOrientationProperty.PORTRAIT
 #_style[] = 20
-# _style[] = 10
+# _style[] = 10font_effects = style['font-effect']
 # _style[] = 20
 # _style[] = 20
 # _style[] = 12
@@ -285,3 +203,182 @@ class OdtBuilder(Builder):
 # _style[] = 12
 # _style[] = True
 # _style[] = 1
+
+class OdtStyleManager(object):
+    def __init__(self, document):
+        self.__effective_style = document.effective_style
+        
+        self.__defaultTextIndent = None
+        self.__defaultFontSize = None
+        self.__defaultFontName = None
+        self.__defaultAlignment = None
+        self.__defaultColor = None
+        self.__defaultBackgroundColor = None
+        self.__defaultListStyle = None
+        self.__defaultItemSpacing = None
+        self.__defaultItemIndent = None
+        self.__defaultBorderWidth = None
+        
+        self.__pageSize = self.__getStyleValue(self.__effective_style, 'page-size')
+        self.__orientation = self.__getStyleValue(self.__effective_style, 'page-orientation')
+        self.__defaultAlignment = self.__getStyleValue(self.__effective_style, 'alignment')
+        
+        self.__defaultTextIndent = self.getTextIndent(self.__effective_style)
+        self.__defaultFontSize = self.getFontSize(self.__effective_style)
+        self.__defaultFontName = self.getFontName(self.__effective_style)
+        self.__defaultColor = self.getColor(self.__effective_style)
+        self.__defaultBackgroundColor = self.getBackgroundColor(self.__effective_style)
+        self.__defaultListStyle = self.getListStyle(self.__effective_style)
+        self.__defaultItemSpacing = self.getItemSpacing(self.__effective_style)
+        self.__defaultItemIndent = self.getItemIndent(self.__effective_style)
+        self.__defaultBorderWidth = self.getBorderWidth(self.__effective_style)
+        
+    def getMarginTop(self, style):
+        return self.__getAttributeMM(style, 'margin-top', 'fo:margin-top');
+    
+    def getMarginBottom(self, style):
+        return self.__getAttributeMM(style, 'margin-bottom', 'fo:margin-bottom');
+    
+    def getMarginLeft(self, style):
+        return self.__getAttributeMM(style, 'margin-left', 'fo:margin-left');
+    
+    def getMarginRight(self, style):
+        return self.__getAttributeMM(style, 'margin-right', 'fo:margin-right');
+    
+    def getTextIndent(self, style):
+        return self.__getAttributeMMOrDefault(style, 'text-indent', 'fo:text-indent', self.__defaultTextIndent)
+    
+    def getFontSize(self, style):
+        return self.__getAttributePTOrDefault(style, 'font-size', 'TODO', self.__defaultFontSize)
+
+    def getFontName(self, style):
+        return self.__getAttributeOrDefault(style, 'font-name', 'TODO', self.__defaultFontName)
+    
+    def getAlignment(self, style):
+        alignment = self.__getStyleValueOrDefault(style, 'alignment', self.__defaultAlignment)
+        if alignment is None:
+            return ''
+        else:
+            if alignment == AlignmentProperty.LEFT:
+                return '' # left is default
+            elif alignment == AlignmentProperty.CENTER:
+                return 'fo:text-align="center" '
+            elif alignment == AlignmentProperty.RIGHT:
+                return 'fo:text-align="end" '
+            elif alignment == AlignmentProperty.JUSTIFY:
+                return 'fo:text-align="justify" '
+            else:
+                return ''
+    
+    def getColor(self, style):
+        return self.__getAttributeOrDefault(style, 'color', 'TODO', self.__defaultColor)
+    
+    def getBackgroundColor(self, style):
+        return self.__getAttributeOrDefault(style, 'background-color', 'TODO', self.__defaultBackgroundColor)
+    
+    def getListStyle(self, style):
+        return self.__getAttributeOrDefault(style, 'list-style', 'TODO', self.__defaultListStyle)
+    
+    def getItemSpacing(self, style):
+        return self.__getAttributeMMOrDefault(style, 'item-spacing', 'TODO', self.__defaultItemSpacing)
+    
+    def getItemIndent(self, style):
+        return self.__getAttributeMMOrDefault(style, 'item-indent', 'TODO', self.__defaultItemIndent)
+    
+    def getHeaderNumbered(self, style):
+        return self.__getStyleValue(style, 'header-numbered')
+    
+    def getBorderWidth(self, style):
+        return self.__getAttributeOrDefault(style, 'border-width', 'TODO', self.__defaultBorderWidth)
+    
+    def getPageNumbering(self, style):
+        return self.__getAttribute(style, 'page-numbering', 'TODO', 'TODO')
+    
+    def getFontEffect(self, style):
+        font_effects = self.__getStyleValue(style, 'font-effect')
+        styleBody = ''
+        if font_effects is not None:
+            if FontEffectProperty.BOLD in font_effects:
+                styleBody += 'fo:font-weight="bold" style:font-weight-asian="bold" style:font-weight-complex="bold" '
+            if FontEffectProperty.ITALIC in font_effects:
+                styleBody += 'fo:font-style="italic" style:font-style-asian="italic" style:font-style-complex="italic" '
+            elif FontEffectProperty.UNDERLINE in font_effects:
+                styleBody += 'style:text-underline-style="solid" style:text-underline-width="auto" style:text-underline-color="font-color" '
+        return styleBody
+    
+    def getPageSize(self, style):
+        styleAttributes = ''
+        if self.__orientation == PageOrientationProperty.PORTRAIT:
+            styleAttributes += 'fo:page-width="' + str(self.__pageSize.value[0]) + 'mm" '
+            styleAttributes += 'fo:page-height="' + str(self.__pageSize.value[1]) + 'mm" ' 
+        else:
+            styleAttributes += 'fo:page-width="' + str(self.__pageSize.value[1]) + 'mm" '
+            styleAttributes += 'fo:page-height="' + str(self.__pageSize.value[0]) + 'mm" ' 
+            
+        return styleAttributes
+    
+    def getPageOrientation(self, style):
+        if self.__orientation == PageOrientationProperty.PORTRAIT:
+            return 'style:print-orientation="portrait" '
+        else:
+            return 'style:print-orientation="landscape" '
+        
+    def getParagraphStyles(self, style):
+        return self.getTextIndent(style) + self.getMarginBottom(style) \
+                + self.getMarginLeft(style) + self.getMarginRight(style) \
+                + self.getMarginTop(style) + self.getAlignment(style)
+    
+    def getTextStyles(self, style):
+        return self.getFontEffect(style)
+    
+    def getPageLayoutStyles(self):
+        style = self.__effective_style
+        return self.getPageNumbering(style) + self.getPageSize(style) \
+                + self.getPageOrientation(style) + self.getMarginTop(style)\
+                + self.getMarginBottom(style) + self.getMarginLeft(style)\
+                + self.getMarginRight(style)
+    
+    def __getAttributeOrDefault(self, style, name, nativeName, default):
+        return self.__getAttributeWithSuffixOrDefault(style, name, nativeName, default, '')
+    
+    def __getAttributePTOrDefault(self, style, name, nativeName, default):
+        return self.__getAttributeWithSuffixOrDefault(style, name, nativeName, default, 'pt')
+    
+    def __getAttributeMMOrDefault(self, style, name, nativeName, default):
+        return self.__getAttributeWithSuffixOrDefault(style, name, nativeName, default, 'mm')
+    
+    def __getAttributeWithSuffixOrDefault(self, style, name, nativeName, default, valueSuffix):
+        att = self.__getAttribute(style, name, nativeName, valueSuffix)
+        if att == '':
+            if default is None:
+                return ''
+            else:
+                return default
+        else:
+            return att
+    
+    def __getStyleValueOrDefault(self, style, name, default):
+        value = self.__getStyleValue(style, name)
+        if value is None:
+            if default is None:
+                return ''
+            else:
+                return default
+        else:
+            return value
+    
+    def __getStyleValue(self, style, name):
+        value = None
+        if style.has_key(name):
+            value = style[name]
+        return value
+        
+    def __getAttributeMM(self, style, name, nativeName):
+        return self.__getAttribute(style, name, nativeName, 'mm')
+    
+    def __getAttribute(self, style, name, nativeName, valueSuffix):
+        att = ''
+        if style.has_key(name):
+            value = str(style[name]) + valueSuffix
+            att = nativeName + '="' + value + '" '
+        return att
